9717c532f1ee15d2a1cc5b96dcfc5804db40c04a
Harshal Patil <you@example.com>
2021-06-04 03:48:53 -0700

    algorithm changes

--- src/my_robot.h
+++ src/my_robot.h
@@ -14,7 +14,9 @@ namespace {
         void entry(const Event& e) {}
         void during() {
             track_velocity(10, 0);
-            if (sensor_value(0) < 10) {
+            std::cout << "sensor" << sensor_value(0) << std::endl;
+            if (sensor_value(0) < 30) {
+                std::cout << "movingforward->rorate state change"<<"angle" <<angle()<< std::endl;
                 emit(Event("tick"));
             }
         }
@@ -25,14 +27,41 @@ namespace {
     public:
         void entry(const Event& e) { }
         void during() {
-            track_velocity(0, 1);
-            if (this->angle() > 1.56) {
-                std::cout << this->angle() << std::endl;
+            if (lock == 0) {
+                lock = 1;
+                if (sensor_value(1) > sensor_value(2)) {
+                    direction = 0;
+                    total_angle += 1.5708;
+
+                }
+                else if (sensor_value(2) > sensor_value(1)) {
+                    direction = 1;
+                    total_angle -= 1.5708;
+                }
+            }
+            if ((direction == 0)&&(angle()<= total_angle)){
+                track_velocity(0, 1.5708);
+                
+            }
+            else if ((direction == 1) && (angle() <= total_angle)) {
+                track_velocity(0, -1.5708);
+                
+            }
+            std::cout << "angle"<<this->angle()<<"total_angle"<< total_angle << std::endl;
+            if (angle() >= total_angle) {
+                std::cout << "rotate->movingforward state change" << std::endl;
+                lock = 0;
                 emit(Event("tick"));
             }
+           
         }
         void exit(const Event& e) {}
+    private:
+        float total_angle = 0;
+        int lock = 0,direction=0;
     };
+    
+
 
     class WandererController : public StateMachine, public AgentInterface {
 
