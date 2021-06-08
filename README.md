5dc82a67e58f7725a06961915cfeab5ed6420894
Harshal Patil <you@example.com>
2021-06-08 04:23:31 -0700

    README edits

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# EEP_520_Project
+# Maze Solving Autonomous Robot
+To put a robot at the start and have it explore until it finds the end of the maze using range sensors, wall following behaviors
--- src/my_robot.h
+++ src/my_robot.h
@@ -5,90 +5,121 @@
 #include <math.h>
 #include "enviro.h"
 
+double total_angle = 0;
 namespace {
 
-    using namespace enviro;
+	using namespace enviro;
 
-    class MovingForward : public State, public AgentInterface {
-    public:
-        void entry(const Event& e) {}
-        void during() {
-            track_velocity(20, 0);
-            std::cout << "sensor" << sensor_value(0) << std::endl;
-            if (sensor_value(0) < 25) {
-                std::cout << "movingforward->rotate state change"<<"angle" <<angle()<< std::endl;
-                emit(Event("tick"));
-            }
-        }
-        void exit(const Event& e) {}
-    };
+	//! Class MovingForward inheritates from State and AgentInterface class
+	//! The State MovingForward is used when the agent is going forward 
+	class MovingForward : public State, public AgentInterface {
+	public:
 
-    class Rotating : public State, public AgentInterface {
-    public:
-        void entry(const Event& e) { }
-        void during() {
-            if (lock == 0) {
-                lock = 1;
-                if (sensor_value(1) > sensor_value(2)) {
-                    direction = 0;
-                    total_angle += 1.5708;
+		//! Initiate and retrieve current angle of the agent 
+		//! \param e Event 
+		void entry(const Event& e) {
+			total_angle = angle();
+		}
 
-                }
-                else if (sensor_value(2) > sensor_value(1)) {
-                    direction = 1;
-                    total_angle -= 1.5708;
-                }
-            }
-            if ((direction == 0)&&(angle()<= total_angle)){
-                track_velocity(0, 0.5236);
-                
-            }
-            else if ((direction == 1) && (angle() >= total_angle)) {
-                track_velocity(0, -0.5236);
-                
-            }
-            std::cout << "angle"<<this->angle()<<"total_angle"<< total_angle << std::endl;
-            if ((direction==0&&angle() >= total_angle)|| (direction == 1 && angle() <= total_angle)) {
-        
-                std::cout << "rotate->movingforward state change" << std::endl;
-                lock = 0;
-                emit(Event("tick"));
-            }
-           
-        }
-        void exit(const Event& e) {}
-    private:
-        double total_angle = 0;
-        int lock = 0,direction=0;
-    };
-    
+		//! moves agent with constant velocity until sensor value reaches threshold
+		//! triggers event to change state to rotating  
+		void during() {
+			track_velocity(25, 0);
+			std::cout << "sensor" << sensor_value(0) << std::endl;
+			if (sensor_value(0) < 25) {
+				std::cout << "movingforward->rotate state change" << "angle" << angle() << std::endl;
+				emit(Event("tick"));
+			}
+		}
+		//! exit MovingForward state
+		//! \param e Event
+		void exit(const Event& e) {}
+	};
+
+	//! Class Rotating inheritates from State and AgentInterface class
+	//! The State MovingForward is used when the agent is going forward 
+	class Rotating : public State, public AgentInterface {
+	public:
+		//! Enter Rotating state
+		//! \param e Event
+		void entry(const Event& e) { }
+
+		//! rotates agent with and an angleo 0f 1.5708 radians or -1.5708 depending on the sensor values
+		//! to precise with the angle, agent's angle is checked before triggering event to change state to MovingForward   
+		void during() {
+			if (lock == 0) {
+				lock = 1;
+				if (sensor_value(1) > sensor_value(2)) {
+					direction = 0;
+					total_angle += 1.5708;
+
+				}
+				else if (sensor_value(2) > sensor_value(1)) {
+					direction = 1;
+					total_angle -= 1.5708;
+				}
+			}
+			if ((direction == 0) && (angle() <= total_angle)) {
+				track_velocity(0, 1.5708);
+			}
+			else if ((direction == 1) && (angle() >= total_angle)) {
+				track_velocity(0, -1.5708);
+			}
+			std::cout << "angle" << this->angle() << "total_angle" << total_angle << std::endl;
+			if ((direction == 0 && angle() >= total_angle) || (direction == 1 && angle() <= total_angle)) {
+				auto pos_agent = position();
+				while (angle() != total_angle)
+				{
+					teleport(pos_agent.x, pos_agent.y, total_angle);
+				}
+				std::cout << "rotate->movingforward state change" << std::endl;
+				lock = 0;
+				emit(Event("tick"));
+			}
+
+		}
+		//! exit MovingForward state
+		//! \param e Event
+		void exit(const Event& e) {}
+
+	private:
+
+		int lock = 0, direction = 0;
+	};
 
 
-    class WandererController : public StateMachine, public AgentInterface {
+	//! Class RobotController inheritates from StateMachine and AgentInterface class
+	//! The class RobotController is set initial state to moveforward and to add_transition function 
+	//! triggered via events
+	class RobotController : public StateMachine, public AgentInterface {
 
-    public:
-        WandererController() : StateMachine() {
-            set_initial(moving_forward);
-            add_transition("tick", moving_forward, rotating);
-            add_transition("tick", rotating, moving_forward);
-        }
+	public:
+		//! Constructor setting initial movement to forward
+		RobotController() : StateMachine() {
+			set_initial(moving_forward);
+			add_transition("tick", moving_forward, rotating);
+			add_transition("tick", rotating, moving_forward);
+		}
 
-        MovingForward moving_forward;
-        Rotating rotating;
-    };
+		MovingForward moving_forward;
+		Rotating rotating;
+	};
 
-    class Wanderer : public Agent {
+	//! Class Controller inheritates from Agent class
+	//! The class RobotController adds a process and creates RobotController object
+	class Controller : public Agent {
 
-    public:
-        Wanderer(json spec, World& world) : Agent(spec, world) {
-            add_process(wc);
-        }
+	public:
+		//! Constructor adds a process
+		Controller(json spec, World& world) : Agent(spec, world) {
+			add_process(wc);
+		}
 
-        WandererController wc;
+		RobotController wc;
 
-    };
+	};
 
-    DECLARE_INTERFACE(Wanderer);
+	DECLARE_INTERFACE(Controller);
 
 }
 
